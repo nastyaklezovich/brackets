108935da767db7e860fcf7f083244123507d9ad5
Klezovich Anastasiya <nastyaklezovich@gmail.com>
2020-10-17 22:56:07 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  let stack = [];
+  let brackets = "";
+  let flag = true;
+
+
+  for (let items of bracketsConfig) {
+      for (let item of items) {
+          brackets += item;
+      }
+  }
+
+  for (let bracket of str) {
+
+      let bracketsIndex = brackets.indexOf(bracket);
+      let lastBracketsIndex = brackets.lastIndexOf(bracket);
+
+      if (bracketsIndex === -1) {
+          continue;
+      }
+      
+      if (bracketsIndex === lastBracketsIndex){
+          if (bracketsIndex % 2 === 0) {
+              stack.push(bracketsIndex + 1);
+          } else {
+              if (stack.pop() !== bracketsIndex) {
+                  return false;
+              }
+          }
+      }
+      else{
+          let n = stack.pop();
+          if (n !== lastBracketsIndex) {
+              if (n !== undefined){
+                  stack.push(n);
+              }
+              stack.push(bracketsIndex + 1);
+          } 
+      }
+  }
+  return stack.length === 0;
+};
